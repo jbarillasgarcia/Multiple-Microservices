26ecc259279081358d9402eacef59d2dd14ed5ba
abarillas <abarillas@nbwaudev04>
2019-04-09 18:44:18 -0600

    Version inicial de la suma.

---
+++ multiservicios-suma/src/main/java/gt/edu/umg/ingenieria/sistemas/dockers/multiservices/suma/SumaApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.dockers.multiservices.suma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumaApplication.class, args);
+	}
+
+}
---
+++ multiservicios-suma/src/test/java/gt/edu/umg/ingenieria/sistemas/dockers/multiservices/suma/SumaApplicationTests.java
@@ -0,0 +1,16 @@
+package gt.edu.umg.ingenieria.sistemas.dockers.multiservices.suma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SumaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
