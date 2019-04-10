533a3ed7466145cd9935317ae038f4662a801524
abarillas <abarillas@nbwaudev04>
2019-04-09 18:43:57 -0600

    Version inicial de la raiz cuadrada.

---
+++ multiservicios-aizcuadrada/src/main/java/gt/edu/umg/ingenieria/sistemas/dockers/multiservices/raizcuadrada/RaizcuadradaApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.dockers.multiservices.raizcuadrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizcuadradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizcuadradaApplication.class, args);
+	}
+
+}
